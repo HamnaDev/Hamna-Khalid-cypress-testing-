60f0951b79f787367e4e85bb1a67fbb0f32b6fff
HamnaDev <hamnakhalid1293@gmail.com>
2025-05-02 03:35:17 +0500

    Delete cypress directory

--- cypress/integration/test.spec.js
+++
@@ -1,9 +0,0 @@
-
-  
-it('should login to the website', () =>{
-cy.visit('https://www.saucedemo.com')
-cy.get('[data-test="username"]').type('admin')
-cy.get('[data-test="password"]').type('admin123')
-cy.get('[data-test="login-button"]').click()
-
-})
